edae7cfd1deecaa254c2ef6e2a48d15d922ffb62
Marcos Querino <mqjunioretecgv@gmail.com>
2021-06-29 23:01:22 -0300

    agora tem malloc - fread não funfa

--- login.c
+++ login.c
@@ -2,15 +2,15 @@
 #include "biblio_geral.h"
 
 /* VARIÁVEIS GLOBAIS */
-secoes ONG[66];                                                             /*vetor de estruturas, mas tenho que usar MALLOC!*/
-int i;         /*contador do for*/
+secoes * ONG;/*ponteiro de estruturas*/
+int        i;/*contador do for*/
 
 /* FUNCÕES */
 void consulta()
-{
-	for(i=0; i<66; i++)                                                           /*como deixar um espaço padronizado?*/
+{	
+	for (i=0; i<66; i++)                                                           /*como deixar um espaço padronizado?*/
 	{
-		printf("\n%s", ONG[i].Entidade);                                          /*como deixar a aparência bonita?*/
+		printf("\n%s" , ONG[i].Entidade);                                          /*como deixar a aparência bonita?*/
 		printf(" %s |", ONG[i].CNPJ);
 		printf(" %s |", ONG[i].Email);
 		printf(" %s |", ONG[i].Telefone);
@@ -23,45 +23,38 @@ void consulta()
 }
 
 
-void ordena_enti() /*ordena entidades usando insertion short*/
-{
-	FILE * dat = fopen("OSC.dat", "rb");    /*abre .dat como binário*/
-	fread(&ONG, sizeof(ONG), 1, dat);       /*copia valores do .dat pra a estrutura*/
-	
-	//insertion short
-	secoes escolhido;
-    int   anterior;
-    for (i = 1; i <66; i++) 
-    {
-		escolhido = ONG[i]; 
-		anterior  = i - 1;
-		while ( ONG[anterior].CNPJ > ONG[i].CNPJ ) 
-		{
-	    	ONG[i] = ONG[anterior];                                                            /*por que não funciona?*/
-	    	ONG[anterior] = escolhido;
-		}
-    }
-    
-    fclose(dat); /*fecha .dat*/
-    
-}
-
 /* CORPO DO PROGRAMA */
 int main()
 {
-	decoracao(); /*habilita acento e cor do programa*/
+	//cor da tela e acentos
+	decoracao();
 	
+	//variáveis locais
 	char email[email_max  + 1];
 	char cnpj [cnpj_max   + 1];
+	
+	//malloc da estrutura
+	FILE * dat = fopen("OSC.dat", "rb");/*abre .dat como binário*/
+	erro_fopen(dat);                    /*verifica erro do fopen*/
+	fseek(dat, 0, SEEK_END);            /*para o ftell funcionar*/
+	ONG = (secoes*) malloc(ftell(dat)); /*malloc da estrutura*/
+	fclose(dat); erro_malloc(ONG);      /*verifica erro do malloc*/
+
+
+	//preenche estrutura - ERRO: fread não vê nada pq fseek levou para o fim do arquivo!
+	limpa_estrutura_malloc(ONG);        /*limpa estrutura*/
+	fread(&ONG, sizeof(ONG), 1, dat);   /*copia valores do .dat pra a estrutura*/
+	fclose(dat);                        /*fecha .dat*/
 	
 	//menu
 	printf("AÇÃO SOCIAL CIDADE SOLIDÁRIA\n");
 	printf("----------------------------\n");
 	printf("digite seu email: ");                fflush(stdin);  gets(email);
 	printf("Digite o cnpj [somente números]: "); fflush(stdin);  gets(cnpj);
-	ordena_enti();
+
+	consulta(); /*chama a consulta*/
 	
-	consulta();
+	free(ONG); /*libera o espaço alocado*/
 	
 	return(0);
 }
