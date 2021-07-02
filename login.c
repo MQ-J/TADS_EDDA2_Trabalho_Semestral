87936e62e34d3fbd31b45508efb71cf83f2c3f76
Marcos Querino <mqjunioretecgv@gmail.com>
2021-07-02 00:10:02 -0300

    login modificado pelo teste

--- login.c
+++ login.c
@@ -3,21 +3,22 @@
 
 /* VARIÁVEIS GLOBAIS */
 secoes * ONG;/*ponteiro de estruturas*/
-int        i;/*contador do for*/
 
 /* FUNCÕES */
 void consulta()
-{	
-	for (i=0; i<66; i++)                                                           /*como deixar um espaço padronizado?*/
+{
+	int i, j;
+	
+	for (i=0; i<66; i++)
 	{
-		printf("\n%s" , ONG[i].Entidade);                                          /*como deixar a aparência bonita?*/
-		printf(" %s |", ONG[i].CNPJ);
-		printf(" %s |", ONG[i].Email);
-		printf(" %s |", ONG[i].Telefone);
-		printf(" %s |", ONG[i].Comunidade);
-		printf(" %s |", ONG[i].Endereco);
-		printf(" %s |", ONG[i].Tipo);
-		printf(" %s |", ONG[i].Subpre);
+		for(j=0; j<  enti_max; j++) printf("\n\n%c " , ONG[i].Entidade[j]);
+		for(j=0; j<  cnpj_max; j++) printf(" %c |", ONG[i].CNPJ[j]);
+		for(j=0; j< email_max; j++) printf(" %c |", ONG[i].Email[j]);
+		for(j=0; j<telefo_max; j++) printf(" %c |", ONG[i].Telefone[j]);
+		for(j=0; j<comuni_max; j++) printf(" %c |", ONG[i].Comunidade[j]);
+		for(j=0; j<endere_max; j++) printf(" %c |", ONG[i].Endereco[j]);
+		for(j=0; j<  tipo_max; j++) printf(" %c |", ONG[i].Tipo[j]);
+		for(j=0; j<subpre_max; j++) printf(" %c |", ONG[i].Subpre[j]);
 		printf(" %i |", ONG[i].Quant_cesta);
 	}
 }
@@ -34,15 +35,13 @@ int main()
 	char cnpj [cnpj_max   + 1];
 	
 	//malloc da estrutura
-	FILE * dat = fopen("OSC.dat", "rb");/*abre .dat como binário*/
-	erro_fopen(dat);                    /*verifica erro do fopen*/
-	fseek(dat, 0, SEEK_END);            /*para o ftell funcionar*/
-	ONG = (secoes*) malloc(ftell(dat)); /*malloc da estrutura*/
-	fclose(dat); erro_malloc(ONG);      /*verifica erro do malloc*/
+	FILE * dat = fopen("OSC.dat", "rb"); erro_fopen(dat); /*abre .dat como binário*/
+	fseek(dat, 0, SEEK_END);                              /*para o ftell funcionar*/
+	ONG = (secoes*) malloc(ftell(dat)); erro_malloc(ONG); /*malloc da estrutura*/
 
-
-	//preenche estrutura - ERRO: fread não vê nada pq fseek levou para o fim do arquivo!
+	//preenche estrutura
 	limpa_estrutura_malloc(ONG);        /*limpa estrutura*/
+	fseek(dat, -ftell(dat), SEEK_END);  /*para o fread funcionar*/
 	fread(&ONG, sizeof(ONG), 1, dat);   /*copia valores do .dat pra a estrutura*/
 	fclose(dat);                        /*fecha .dat*/
 	
