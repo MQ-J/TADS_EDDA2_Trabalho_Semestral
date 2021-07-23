106500f94c77433e0c64c1a0316e5878dbd58513
Marcos Querino <mqjunioretecgv@gmail.com>
2021-07-22 22:15:56 -0300

    separando as funções

--- MENU.c
+++ MENU.c
@@ -1,8 +1,108 @@
 /* BIBLIOTECA */
 #include "biblio_geral.h"
 
+/* VARIÁVEIS GLOBAIS */
+secoes * ONG; /*ponteiro de estruturas*/
+int quant;    /*quantidade de organizações no .dat*/
+
 /* FUNCÕES */
-int login(secoes* ONG, int quant, char email[email_max  + 1], char cnpj [cnpj_max   + 1])
+void organiza_entidade(void);
+int login(int quant, char email[email_max  + 1], char cnpj [cnpj_max   + 1]);
+void consulta(secoes* ONG, int quant);
+
+/* CORPO DO PROGRAMA */
+int main()
+{
+	//cor da tela e acentos
+	decoracao();
+	
+	//variáveis locais
+	char email[email_max  + 1]; /*para login no sistema*/
+	char cnpj [cnpj_max   + 1]; /*para login no sistema*/
+	int op, loop;               /*controla opção do sistema*/
+	
+	//faz malloc e organiza entidades
+	organiza_entidade();
+	
+	//menu de login
+	do
+	{
+		system("cls");
+		printf("AÇÃO SOCIAL CIDADE SOLIDÁRIA\n");
+		printf("----------------------------\n");
+		printf("Digite seu email: ");                fflush(stdin);  gets(email);
+		printf("Digite o cnpj [somente números]: "); fflush(stdin);  gets(cnpj);
+	}
+	while(login(quant, email, cnpj) != 1);
+	
+	//menu de opções
+	do
+	{
+		organiza_entidade();
+		system("cls");
+		printf("----------------------------\n");
+		printf("AÇÃO SOCIAL CIDADE SOLIDÁRIA\n");
+		printf("----------------------------\n");
+		printf("1 - Consulta de entidades\n");
+		printf("2 - Cadastro de entidades\n");
+		printf("3 - Cadastro de assistidos\n");
+		printf("4 - Cadastro de donativos\n");
+		printf("5 - Atendimento\n");
+		printf("0 - sair\n");
+		printf("9 - reinicia cadastro de estruturas\n"); /*função momentãnea para testes*/
+		printf("----------------------------\n");
+		printf("Digite a opção escolhida: ");
+		fflush(stdin); scanf("%i", &op);
+		switch(op)
+		{
+			case 1: consulta(ONG, quant); break;
+			case 2: system("cadastra_entidade.exe"); break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			case 9: system("converte_csv-dat.exe"); break;
+			case 0: loop = 1; break;
+		}
+	}
+	while(loop != 1);
+	
+	return(0);
+}
+
+void organiza_entidade()
+{
+	//malloc da estrutura
+	FILE * dat = fopen("OSC.dat", "rb"); erro_fopen(dat); /*abre .dat como binário*/
+	fseek(dat, 0, SEEK_END);                              /*para o ftell funcionar*/
+	ONG = (secoes*) malloc(ftell(dat)); erro_malloc(ONG); /*malloc da estrutura*/
+	
+	//define numero de estruturas
+	quant = ftell(dat) / sizeof(secoes);   //printf("numero de orgs: %i\n\n", quant); getch();
+	
+	//limpa estrutura
+	memset(ONG->Entidade,   '\0', sizeof(ONG->Entidade));
+	memset(ONG->CNPJ,       '\0', sizeof(ONG->CNPJ));
+	memset(ONG->Email,      '\0', sizeof(ONG->Email));
+	memset(ONG->Telefone,   '\0', sizeof(ONG->Telefone));
+	memset(ONG->Comunidade, '\0', sizeof(ONG->Comunidade));
+	memset(ONG->Endereco,   '\0', sizeof(ONG->Endereco));
+	memset(ONG->Tipo,       '\0', sizeof(ONG->Tipo));
+	memset(ONG->Subpre,     '\0', sizeof(ONG->Subpre));
+	ONG->Quant_cesta = 0;
+
+	//preenche estrutura
+	fseek(dat, 0, SEEK_SET);                /*para o fread funcionar*/
+	fread(ONG, sizeof(secoes), quant, dat); /*copia valores do .dat pra a estrutura*/
+	fclose(dat);                            /*fecha .dat*/
+	
+	//ordena estrutura
+	ordena_enti(ONG, 0, quant-1);
+	
+	//libera o espaço alocado
+	free(ONG);
+}
+
+int login(int quant, char email[email_max  + 1], char cnpj [cnpj_max   + 1])
 {
 	int metade = quant/2; /*para ler o meio da estrutura*/
 	int libera;           /*variável que controla o acesso*/
@@ -25,16 +125,15 @@ int login(secoes* ONG, int quant, char email[email_max  + 1], char cnpj [cnpj_ma
 
 	if (strcmp(cnpj, ONG[metade].CNPJ) < 0) /*caso cnpj seja menor*/
 	{
-		libera = login(ONG, metade-1, email, cnpj);
+		libera = login(metade-1, email, cnpj);
 	}
 	if (strcmp(cnpj, ONG[metade].CNPJ) > 0) /*caso cnpj seja maior*/
 	{
-		libera = login(ONG, quant+3, email, cnpj);
+		libera = login(quant+3, email, cnpj);
 	}
 	
 	return(libera);
 }
-
 
 void consulta(secoes* ONG, int quant)
 {
@@ -54,88 +153,4 @@ void consulta(secoes* ONG, int quant)
 	}
 	printf("\n\nDigite qualquer tecla para voltar");
 	getche();
-}
-
-
-/* CORPO DO PROGRAMA */
-int main()
-{
-	//cor da tela e acentos
-	decoracao();
-	
-	//variáveis locais
-	secoes * ONG;               /*ponteiro de estruturas*/
-	int quant;                  /*quantidade de organizações no .dat*/
-	char email[email_max  + 1]; /*para login no sistema*/
-	char cnpj [cnpj_max   + 1]; /*para login no sistema*/
-	int op, loop;               /*controla opção do sistema*/
-	
-	//malloc da estrutura
-	FILE * dat = fopen("OSC.dat", "rb"); erro_fopen(dat); /*abre .dat como binário*/
-	fseek(dat, 0, SEEK_END);                              /*para o ftell funcionar*/
-	ONG = (secoes*) malloc(ftell(dat)); erro_malloc(ONG); /*malloc da estrutura*/
-	
-	//define numero de estruturas
-	quant = ftell(dat) / sizeof(secoes);   /*debug printf("numero de orgs: %i\n\n", quant);*/
-	
-	//limpa estrutura
-	memset(ONG->Entidade,   '\0', sizeof(ONG->Entidade));
-	memset(ONG->CNPJ,       '\0', sizeof(ONG->CNPJ));
-	memset(ONG->Email,      '\0', sizeof(ONG->Email));
-	memset(ONG->Telefone,   '\0', sizeof(ONG->Telefone));
-	memset(ONG->Comunidade, '\0', sizeof(ONG->Comunidade));
-	memset(ONG->Endereco,   '\0', sizeof(ONG->Endereco));
-	memset(ONG->Tipo,       '\0', sizeof(ONG->Tipo));
-	memset(ONG->Subpre,     '\0', sizeof(ONG->Subpre));
-	ONG->Quant_cesta = 0;
-
-	//preenche estrutura
-	fseek(dat, 0, SEEK_SET);                /*para o fread funcionar*/
-	fread(ONG, sizeof(secoes), quant, dat); /*copia valores do .dat pra a estrutura*/
-	fclose(dat);                            /*fecha .dat*/
-	
-	//ordena estrutura
-	ordena_enti(ONG, 0, quant-1);
-	
-	//menu de login
-	do
-	{
-		system("cls");
-		printf("AÇÃO SOCIAL CIDADE SOLIDÁRIA\n");
-		printf("----------------------------\n");
-		printf("Digite seu email: ");                fflush(stdin);  gets(email);
-		printf("Digite o cnpj [somente números]: "); fflush(stdin);  gets(cnpj);
-	}
-	while(login(ONG, quant, email, cnpj) != 1);
-	
-	//menu de opções
-	do
-	{
-		system("cls");
-		printf("AÇÃO SOCIAL CIDADE SOLIDÁRIA\n");
-		printf("----------------------------\n");
-		printf("1 - Consulta de entidades\n");
-		printf("2 - Cadastro de entidades\n");
-		printf("3 - Cadastro de assistidos\n");
-		printf("4 - Cadastro de donativos\n");
-		printf("5 - Atendimento\n");
-		printf("0 - sair\n");
-		printf("----------------------------\n");
-		printf("Digite a opção escolhida: ");
-		fflush(stdin); scanf("%i", &op);
-		switch(op)
-		{
-			case 1: consulta(ONG, quant); break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-			case 5: break;
-			case 0: loop = 1; break;
-		}
-	}
-	while(loop != 1);
-	
-	free(ONG); /*libera o espaço alocado*/
-	
-	return(0);
 }
